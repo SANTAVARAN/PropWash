0d4c24ae0e6fa6fe4b26bf3200ac631a42427c80
Арсений <santavaran@gmail.com>
2020-01-04 13:30:09 +0300

    add django

--- mysite/PropProj/views.py
+++ mysite/PropProj/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def hello_world(request):
-    return HttpResponse("Hello, world! You're at the hello index!")
+    return render(request, "index.html")
+    #return HttpResponse("e8c65c56-6bde-4568-9017-b2f848990f98.JPG")
+def index(request):
+    return HttpResponse("Да")
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 
-urlpatterns = [
-    path('PropProj/', include('PropProj.urls')),
-]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+urlpatterns = [
+    path('PropProj/', include('PropProj.urls')),
 ]
