2b2d0ba79c226ee3686587140d985ce69728e018
Арсений <santavaran@gmail.com>
2020-01-04 12:37:23 +0300

    add django

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
 
+urlpatterns = [
+    path('PropProj/', include('PropProj.urls')),
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
