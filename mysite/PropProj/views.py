406eaed5c6c700d9e8fdae431076e641bbf10d0b
Арсений <santavaran@gmail.com>
2020-01-05 18:37:47 +0300

    add django

--- mysite/PropProj/views.py
+++ mysite/PropProj/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Part
+from .models import *
 
 
 def hello_world(request):
     #parts = Part.objects.all()
-    parts = Part.objects.order_by('Type')
-    SetParts=set(parts)
-    Uparts=SetParts
-    return render(request, "configurator.html", {"parts": parts})
+    parts = Part.objects.all()
+    part_types = set(map(lambda x:x.Type, parts[:]))
+    catalog = []
+    for part_type in part_types:
+        current_parts = list(filter(lambda x: x.Type == part_type, parts))
 
+        category_items = []
+        for part in current_parts:
+            current_specs = PartSpecs.objects.filter(PartID=part)
+
+            ready_specs = []
+            for spec in current_specs:
+                name = spec.SpecID.Name
+                value = spec.value
+                ready_specs.append((name, value))
+
+            category_items.append({
+                "name": part.Name,
+                "specs": ready_specs
+            })
+        catalog.append({
+            "name": part_type,
+            "items": category_items
+        })
+    print(catalog)
+
+
+    return render(request, "configurator.html", {"catalog": catalog})
     #if part.Type != part.Type + 1:
 
 
